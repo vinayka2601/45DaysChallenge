e60d958a3420cabf06261462c599b95e51b6fc39
vinayka2601 <vinaykaawasthi26@gmail.com>
2024-07-26 15:44:50 +0530

    removeD

---
+++ removeDupli_A9.java
@@ -0,0 +1,12 @@
+public class removeDupli_A9 {
+    public static int isSorted(int n, int []a) {
+        for(int i=1;i<n;i++){
+            if(a[i]>=a[i-1]){
+               
+            }else{
+                return 0;
+            }
+        } 
+        return 1;
+     }
+}
