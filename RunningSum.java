3fb745069a5f667178741c189480ca1b9e8b8028
vinayka2601 <vinaykaawasthi26@gmail.com>
2024-08-16 14:37:25 +0530

    30th

---
+++ RunningSum.java
@@ -0,0 +1,17 @@
+public class RunningSum {
+    public static int[] runningSum(int[] nums) {
+        int sum[]=new int[nums.length];
+       
+        for(int i=1;i<=nums.length-1;i++){
+            
+            sum[0]=nums[0];
+        nums[i]=nums[i-1]+nums[i];
+        sum[i]=sum[i]+nums[i];
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        int nums[]={1,2,3,4};
+        System.out.println(runningSum(nums));
+    }
+}
