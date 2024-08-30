184e0a898622631a99a774dc447df8da8d53ce92
vinayka2601 <vinaykaawasthi26@gmail.com>
2024-08-30 23:43:33 +0530

    c

--- Divisior.java
+++ Divisior.java
@@ -3,13 +3,18 @@ public class Divisior {
         public static List< Integer > printDivisors(int n) {
             List<Integer> divisors = new ArrayList<>();
             //whenever it gives us LIST we can creat another List and perform the algo
-            for(int div=1;div<=10;div++){
+            for(int div=1;div<=n;div++){
                 if(n%div==0){
+                   // System.out.println(div);
                     divisors.add(div);
                 }
                
             }
              return divisors;
         
+}
+public static void main(String[] args) {
+    int n=50;
+    System.out.println(printDivisors(n));;
 }
 }
