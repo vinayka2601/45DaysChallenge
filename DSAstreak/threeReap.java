e162349292f6f7dd1f2e43e8dec612c359236cb0
vinayka2601 <vinaykaawasthi26@gmail.com>
2024-10-01 23:55:10 +0530

    1 oct

--- DSAstreak/threeReap.java
+++ DSAstreak/threeReap.java
@@ -4,33 +4,28 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class threeReap {
-    public static void fun(int nums[]){
-        //int nums[]=new int[32];
-        int count=0;
-        HashMap <Integer,Integer> hm=new HashMap<>();
-
-       
-         for( int num :nums ){
-            if(hm.containsKey(num)){
-                count++;
-                hm.put(num,count);
+    public static int fun(int nums[],int n){
+         int arr[]=new int[32];
+        
+         for(int i=0;i<n;i++){
+             for(int j=0;j<n;j++){
+                if((nums[j] & (1<<i))!=0){
+                    arr[i]+=1;
+                }
+             }
             }
+            int res=0;
+         for(int i=0;i<arr.length;i++){
+           if(arr[i]%3 !=0){
+            res |=(1<<i);
+           }
          }
-        if(count%3==0){
-          count=0;
-        }else{
-            count=1;
-        }
-
-   hm.forEach((key ,value)->{
-    System.out.println(key+""+value);
-   });
-    
+      return res;
    }
-    
     public static void main(String[] args) {
         int nums[]={2,2,1,5,1,1,2};
-        fun(nums);
+       int n=nums.length;
+       System.out.println(fun(nums, n));
 
     }
 }
