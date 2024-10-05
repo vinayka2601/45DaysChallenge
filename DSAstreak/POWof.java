2687bbb198ff376d35d8b167672073f28835aa75
vinayka2601 <vinaykaawasthi26@gmail.com>
2024-10-05 11:19:05 +0530

    .

---
+++ DSAstreak/POWof.java
@@ -0,0 +1,11 @@
+package DSAstreak;
+
+public class POWof {
+    public boolean isPowerOfTwo(int n) {
+        if(n<=0){
+            return false;
+        }
+            return (n & (n-1))==0;
+        
+    }
+}
